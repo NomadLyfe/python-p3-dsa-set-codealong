d55f452e3e81659edf189d874c29811a8aca7087
Jeremy Garcia <jeremygarcia9400@gmail.com>
2023-08-20 07:32:55 -0400

    complete lab

--- lib/MySet.py
+++ lib/MySet.py
@@ -1,3 +1,31 @@
 class MySet:
+    def __init__(self, enumerable = []):
+        self.dictionary = {}
+        for value in enumerable:
+            self.dictionary[value] = True
 
-    pass
+    def has(self, value):
+        return value in self.dictionary
+
+    def add(self, value):
+        self.dictionary[value] = True
+        return self
+
+    def delete(self, value):
+        self.dictionary.pop(value, None)
+
+    def size(self):
+        return len(self.dictionary)
+    
+    def clear(self):
+        self.dictionary = {}
+        return self.dictionary
+    
+    def __str__(self):
+        string = ''
+        for i, key in enumerate(self.dictionary.keys()):
+            if i == len(self.dictionary.keys())-1:
+                string += f'{key}'
+            else:
+                string += f'{key},'
+        return f'MySet: {{{string}}}'
